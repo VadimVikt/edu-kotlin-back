d76e355dc39a97ba68845c28b8823a3d9b15ea72
BridnyaVV <VVBridnya@sberbank.ru>
2023-02-08 22:59:49 +0300

    mi: feature Начало проекта доработка первого задания - gitignore и gradle.properties [m1-init]

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,25 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.10" apply false
+    kotlin("jvm")
 }
 
 group = "ru.vadim.kotlin-marketplace"
 version = "0.0.1"
 
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
 subprojects {
     group = rootProject.group
     version = rootProject.version
-    repositories {
-        mavenCentral()
-    }
-    tasks.withType<KotlinCompile>() {
+
+    tasks.withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "11"
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,11 @@
-rootProject.name = "kotlin-marketplace"
+rootProject.name = "edu-kotlin-back"
+
+pluginManagement {
+    val kotlinVersion: String by settings
+
+    plugins {
+        kotlin("jvm") version kotlinVersion
+    }
+}
 
 include("m1l1-quickstart")
