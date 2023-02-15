3178b9e69e9a2882dcbe2f66059b572d859f34dc
BridnyaVV <VVBridnya@sberbank.ru>
2023-02-15 16:48:54 +0300

    m2: feature Подготовка документации, среды развертывания среды тестирования доработка [m2-prepare]

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,10 +2,14 @@ rootProject.name = "edu-kotlin-back"
 
 pluginManagement {
     val kotlinVersion: String by settings
+    val kotestVersion: String by settings
 
     plugins {
         kotlin("jvm") version kotlinVersion
+        kotlin("multiplatform") version kotlinVersion
+        id("io.kotest.multiplatform") version kotestVersion apply false
     }
 }
 
 include("m1l1-quickstart")
+include("m2-prepare")
